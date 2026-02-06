c35a88cfb6908099ad897745c5b7a50b96e215ab
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2026-02-06 15:28:22 +0700

    feat: update API base URL to use environment variable for better configuration management

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://127.0.0.1:8000/api";
+const url = import.meta.env.VITE_API_BASE_URL;
 
 // State untuk prevent multiple refresh calls
 let isRefreshing = false;
